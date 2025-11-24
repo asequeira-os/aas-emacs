97fcc4c4eb167515ed96468e8256c5fcdd02f4f2
Antony Sequeira <asequeira.open@gmail.com>
2025-11-23 20:23:55 -0800

    increase font size

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
 ;; (menu-bar-mode -1)
 ;; (scroll-bar-mode -1)
 (setq visible-bell t)
+(set-face-attribute 'default nil :height 160)
 
 ;; emacs driven local setting location
 ;; main file won't be overwritten
